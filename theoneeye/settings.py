e57b07782d8ed172dc959f7888b5e166ae4249a8
RoshanYadav <roshanyadav1st@gmail.com>
2025-10-03 15:18:35 +0000

    Remove portal app and associated files, including database, media assets, and migrations.

--- theoneeye/settings.py
+++ theoneeye/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "workflow",
-    "portal"
 ]
 
 MIDDLEWARE = [
--- theoneeye/urls.py
+++ theoneeye/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from theoneeye import settings
 
 urlpatterns = [
-    path("",include("portal.urls")),
     path("admin/", admin.site.urls),
     path("api/",include("workflow.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
