d822a832231844bc05f89ece28aa768074d465c2
RoshanYadav <roshanyadav1st@gmail.com>
2025-10-06 08:54:22 +0000

    Update StandaloneNodeSerializer to include NodeGroupSerializer; modify Node model to allow blank data field for form_configuration values; update db.sqlite3 to reflect schema changes.

--- workflow/Serializers/StandaloneNode.py
+++ workflow/Serializers/StandaloneNode.py
@@ -1,21 +1,22 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, ImageField, JSONField
 from workflow.models import StandaloneNode
-
+from .NodeGroup import NodeGroupSerializer
 
 class StandaloneNodeSerializer(ModelSerializer):
     # Include node_group information in the response
-    node_group_name = serializers.CharField(source='node_group.name', read_only=True)
-    node_group_icon = serializers.ImageField(source='node_group.icon', read_only=True)
+    # node_group_name = serializers.CharField(source='node_group.name', read_only=True)
+    # node_group_icon = serializers.ImageField(source='node_group.icon', read_only=True)
+    node_group = NodeGroupSerializer()
     
     class Meta:
         model = StandaloneNode
         fields = [
-            'id', 'name', 'type', 'node_group', 'node_group_name', 'node_group_icon',
+            'id', 'name', 'type', 'node_group',
             'description', 'version', 'is_active', 'created_by', 'form_configuration', 'tags', 'logo',
             'created_at', 'updated_at'
         ]
-        read_only_fields = ['id', 'created_at', 'updated_at', 'node_group_name', 'node_group_icon']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
 
 class StandaloneNodeCreateSerializer(ModelSerializer):
